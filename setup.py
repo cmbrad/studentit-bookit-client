dab8288c8d130b7bef75d2b9b7e6a62c300e6afd
Christopher Bradley <Chris.BRADLEY@cy.id.au>
2017-07-31 22:37:05 +1000

    Bump version to 0.1.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(filename):
 
 setup(
     name='studentit-bookit-client',
-    version='0.1.2',
+    version='0.1.3',
     description='API client and command line application for connecting to BookIT',
     long_description=read('README.rst'),
     # The project's main homepage.
